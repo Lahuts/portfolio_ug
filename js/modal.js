e2f1296465ec37baabcc662a2fd20829af1bd925
Lahuts <albumcroquis@gmail.com>
2022-05-08 22:13:14 +0200

    fix

--- js/modal.js
+++ js/modal.js
@@ -9,35 +9,35 @@ document.addEventListener("DOMContentLoaded",e =>{
             <h5>Mes languages de programmation</h5>
             <ul class="comp-dis">
                 <li>
-                    <img src="/asset/js.png" alt="JavaScript">
+                    <img src="asset/js.png" alt="JavaScript">
                     <h6>JAVASCRIPT</h6>
                 </li>
                 <li>
-                    <img src="/asset/java.png" alt="Java">
+                    <img src="asset/java.png" alt="Java">
                     <h6>JAVA</h6>
                 </li>
                 <li>
-                    <img src="/asset/html.png" alt="html">
+                    <img src="asset/html.png" alt="html">
                     <h6>HTML</h6>
                 </li>
                 <li>
-                    <img src="/asset/css.png" alt="css">
+                    <img src="asset/css.png" alt="css">
                     <h6>CSS</h6>
                 </li>
                 <li>
-                    <img src="/asset/C++.png" alt="c++">
+                    <img src="asset/C++.png" alt="c++">
                     <h6>C++</h6>
                 </li>
                 <li>
-                    <img src="/asset/sql.png" alt="sql">
+                    <img src="asset/sql.png" alt="sql">
                     <h6>SQL</h6>
                 </li>
                 <li>
-                    <img src="/asset/python.png" alt="python">
+                    <img src="asset/python.png" alt="python">
                     <h6>PYTHON</h6>
                 </li>
                 <li>
-                    <img src="/asset/c.svg" alt="c#">
+                    <img src="asset/c.svg" alt="c#">
                     <h6>C#</h6>
                 </li>
             </ul>`;
@@ -57,19 +57,19 @@ document.addEventListener("DOMContentLoaded",e =>{
         <h5>Mes logiciels de graphisme </h5>
         <ul class="dis-graph">
             <li>
-                <img src="/asset/Adobe_Photoshop_CC.svg" alt="Photoshop">
+                <img src="asset/Adobe_Photoshop_CC.svg" alt="Photoshop">
                 <h6> Photoshop</h6>
             </li>
             <li>
-                <img src="/asset/illu.svg" alt="Illustrator">
+                <img src="asset/illu.svg" alt="Illustrator">
                 <h6> Illustrator</h6>
             </li>
             <li>
-                <img src="/asset/indesign.svg" alt="Indesign">
+                <img src="asset/indesign.svg" alt="Indesign">
                 <h6> Indesign</h6>
             </li>
             <li>
-                <img src="/asset/figma.png" alt="Figma">
+                <img src="asset/figma.png" alt="Figma">
                 <h6> figma</h6>
             </li>
         </ul>`;
